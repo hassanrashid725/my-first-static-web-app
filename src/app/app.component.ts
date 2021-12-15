35472a4038b0ca8ba5bea89587408c1310d58b4f
hassanrashid725 <hassanrashid725@gmail.com>
2021-12-15 12:42:26 +0100

    create http function for message api and add api call (remaining changes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template: `<span i18n>Welcome</span><div>Hello {{value}}</div>`,
+  template: `<span i18n>Welcome</span><div>Hello {{value}}. Message: {{message}}</div>`,
 })
 export class AppComponent {
   value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
